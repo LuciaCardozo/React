5861b3aff639d304fa6d397ee035082fbd8a1c56
Lucia <lucia.t.cardozo@gmail.com>
2021-01-06 19:58:36 -0300

    algunas mejoras

--- c1/src/components/Contador.jsx
+++ c1/src/components/Contador.jsx
@@ -8,23 +8,29 @@ export default class Contador extends React.Component{
         this.state = {
             valor : 0
         }
+        //bind en el constructor
+        this.inc = this.incrementar.bind(this);
+        this.dec = this.decrementar.bind(this);
     }    
 
-    incrementar = ()=>{
-        
+    incrementar (){
          this.setState({valor: this.state.valor+1});
     }
-    decrementar = ()=>{
+    decrementar (){
         this.setState({valor: this.state.valor-1});
     }
+    //Arrow
+    /*decrementar = ()=>{
+        this.setState({valor: this.state.valor-1});
+    }*/
 
     render(){
         return (
             <div>
                 <h1>Contador</h1>
                 <h3>El valor es: {this.state.valor}</h3>
-                <button onClick={this.incrementar}>Incrementar</button>
-                <button onClick={this.decrementar}>Decrementar</button>
+                <button onClick={this.inc}>Incrementar</button>
+                <button onClick={this.dec}>Decrementar</button>
             </div>
             )
     }
